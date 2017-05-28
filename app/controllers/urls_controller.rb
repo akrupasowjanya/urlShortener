2b6dc445cc3e6f17058cdb210091f3d48307aa9c
dathuk <dathu.k@mdoffice.in>
2017-05-28 20:43:38 +0530

    initial commit for code shortener

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,43 @@
+class UrlsController < ApplicationController
+  before_action :find_url, only: [:show]
+
+  def index
+    @url = Url.new
+  end
+
+  def show
+    redirect_to @url.sanitized_url
+  end
+
+  def create
+    @url = Url.new(url_params)
+    @url.sanitize
+    if @url.new_url?
+      if @url.save
+        # redirect_to shortened_path(@url.short_url)
+      else
+        flash[:error] = "Check the error below:"
+        render 'index'
+      end
+    else
+    	@url = @url.find_duplicate
+      flash[:notice] = "A short link for this URL is already in our database"
+      # redirect_to shortened_path(@url.short_url)
+    end
+  end
+
+  def shortened
+  end
+
+  private
+
+  def find_url
+    @url = Url.find_by_short_url(params[:short_url])
+  end
+
+  def url_params
+    params.require(:url).permit(:original_url)
+  end
+
+
+end
